9d0647d26cb410f2c19c16bfc0a5914a774b9296
spin6lock <john.37@gmail.com>
2015-09-01 22:54:50 +0800

    add mail alert; fix page element not match

--- smzdm_checkin.py
+++ smzdm_checkin.py
@@ -28,6 +28,18 @@ def send_warning_mail():
               "text": "Testing some Mailgun awesomness!",
               "html": '<html>Inline image here: <img src="cid:smzdm.png"></html>'})
 
+def send_simple_mail(content):
+    return requests.post(
+        MAILGUN_API_URL,
+        auth=("api", MAILGUN_KEY),
+        data={"from": MAILFROM,
+              "to": MAILTO,
+              "subject": MAIL_TITLE,
+              "text": content,
+              },
+        )
+
+
 def checkin():
     output_filename = join(dirname(abspath(__file__)), "ret")
     with open(output_filename, "r") as fh:
@@ -44,32 +56,37 @@ def checkin():
     browser.set_window_size(1024, 768)
     browser.get("http://www.smzdm.com")
 
-    element = browser.find_element_by_id("navBar_login")
-    element.click()
+    try:
+        element = browser.find_element_by_id("user_info_tosign")
+        element.click()
 
-    username = browser.find_element_by_id("user_login")
-    password = browser.find_element_by_id("user_pass")
-    login_button = browser.find_element_by_id("btn_login")
+        browser.switch_to_frame("zhiyou_login_window_iframe")
+        username = browser.find_element_by_id("username")
+        password = browser.find_element_by_id("password")
+        login_button = browser.find_element_by_id("login_submit")
 
-    username.send_keys(USERNAME)
-    password.send_keys(PASSWORD)
-    login_button.click()
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
+        login_button.click()
 
-    WebDriverWait(browser, WAITTIME_BEFORE_CLICK).until(
-        lambda x: len(x.find_element_by_id("user_info_score").text) > 0)
-    WebDriverWait(browser, WAITTIME_BEFORE_CLICK).until(
-        ajax_complete, "timeout waiting for page to load")
-    checkin_button = browser.find_element_by_id("user_info_tosign")
-    checkin_button.click()
-    WebDriverWait(browser, WAITTIME_AFTER_CLICK).until(
-        ajax_complete, "timeout waiting for page to load")
-    text_info = browser.find_element_by_id("user_info_tosign").text
-    with open(output_filename, "a") as fh:
-        fh.write(str(datetime.datetime.now()) + "\t" + text_info.encode("utf8")
-                 + "\n")
-    browser.save_screenshot(SCREENSHOT_PATH)
+        WebDriverWait(browser, WAITTIME_BEFORE_CLICK).until(
+            lambda x: len(x.find_element_by_id("user_info_score").text) > 0)
+        WebDriverWait(browser, WAITTIME_BEFORE_CLICK).until(
+            ajax_complete, "timeout waiting for page to load")
+        checkin_button = browser.find_element_by_id("user_info_tosign")
+        checkin_button.click()
+        WebDriverWait(browser, WAITTIME_AFTER_CLICK).until(
+            ajax_complete, "timeout waiting for page to load")
+        text_info = browser.find_element_by_id("user_info_tosign").text
+        with open(output_filename, "a") as fh:
+            fh.write(str(datetime.datetime.now()) + "\t" + text_info.encode("utf8")
+                    + "\n")
+        browser.save_screenshot(SCREENSHOT_PATH)
 
-    browser.quit()
+        browser.quit()
+    except selenium.common.exceptions.NoSuchElementException, err:
+        print err
+        #send_simple_mail(err)
 
 
 checkin()
